dcb204fcbfd3cd7979232fd631d7cf531326f7ca
fatemehTe <fateme.tekrar@gmail.com>
2023-06-15 17:56:19 +0430

    assets added

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ app.use('/thisWeekTasks', thisWeekTasks);
 const CONNECTION_URL = 'mongodb://localhost/radepa'
 
 const PORT = process.env.PORT || 5000;
-app.use('/', express.static(path.resolve(__dirname,'assets')))
+// app.use('/', express.static(path.resolve(__dirname,'assets')))
+app.use('/', connect.static(__dirname + '/assets'));
 mongoose.set('strictQuery',false)
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true,useUnifiedTopology:true })
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
