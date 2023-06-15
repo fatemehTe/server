6ad7df6002f50b3f40717c32ef28064a82dfcb72
fatemehTe <fateme.tekrar@gmail.com>
2023-06-15 16:26:26 +0430

    assets added

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const CONNECTION_URL = 'mongodb+srv://Fateme:misisisimi1@nodeexpressproject.vb2r
 
 // const CONNECTION_URL = 'mongodb://localhost/radepa'
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 27017;
 mongoose.set('strictQuery',false)
 mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true,useUnifiedTopology:true })
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
